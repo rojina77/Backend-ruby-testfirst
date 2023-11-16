94936105e073da5d4eb92f31238961b0be6c2287
rojina pradhan <roselark7@gmail.com>
2023-11-15 19:42:16 -0800

    Corrected codes for TDD

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,11 @@
 #write your code here
+# hello.rb
+
+def hello
+    "Hello!"
+  end
+  
+  def greet(name)
+    "Hello, #{name}!"
+  end
+  
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
   #write your code here
 end
+attr_accessor :seconds
+
+def initialize
+  @seconds = 0 # a Timer class with a seconds attribute
+end
+
+# Define the time_string method that returns the time in the format
+def time_string 
+  # uses integer division and modulo arithmetic to perform the calculation
+  hours = @seconds / 3600
+  minutes = (@seconds % 3600) / 60
+  seconds = @seconds % 60
+  format("%02d:%02d:%02d", hours, minutes, seconds)
+end
